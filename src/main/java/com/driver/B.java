8b7b3222ee7fbfa205688a41b465b8c84756cb7a
Abhay Singh <abhaysingh07082000@gmail.com>
2022-12-27 10:20:39 +0530

    oerrriding

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class B extends A{
+
+    @Override
+    public String meth()
+    {
+        super.meth();
+        return "Method is overridden in Extendend class B";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,10 @@
 package com.driver;
 
-  class A {
-
- public String meth() {
-  return "Invoking method from class A";
- }
-}
-  class B extends A {
-
-
-@Override
-  public String meth()
-  {
-      super.meth();
-   return "Method is overridden in Extendend class B";
-  }
-
- }
 
  public class Main
  {
   public static void main(String[] args) {
+
 
 
     B c=new B();
